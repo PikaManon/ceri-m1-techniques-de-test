fe04d497399faa0b85024b03dcefb1eb8bb1827e
PikaManon <manon.samson@univ-avignon.fr>
2023-02-25 09:56:39 +0100

    tp3 IPokedexFactoryTest

--- src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -1,4 +1,32 @@
 package fr.univavignon.pokedex.api;
 
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.when;
+
 public class IPokedexFactoryTest {
+
+    static IPokemonMetadataProvider pokemonMetadataProvider;
+    static IPokemonFactory pokemonFactory;
+    static IPokedexFactory pokedexFactory;
+    static IPokedex pokedex;
+
+    @BeforeClass
+    public static void setUpClass(){
+        pokemonMetadataProvider = Mockito.mock(IPokemonMetadataProvider.class);
+        pokemonFactory = Mockito.mock(IPokemonFactory.class);
+        pokedexFactory = Mockito.mock(IPokedexFactory.class);
+        pokedex = Mockito.mock(IPokedex.class);
+    }
+
+    @Test
+    public void createPokedexTest(){
+        when(pokedexFactory.createPokedex(pokemonMetadataProvider,pokemonFactory)).thenReturn(pokedex);
+        Assert.assertEquals(pokedexFactory.createPokedex(pokemonMetadataProvider,pokemonFactory).size(), 0);
+    }
+
 }
